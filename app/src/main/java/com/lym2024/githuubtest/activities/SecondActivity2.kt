d403afb816b0c7576d93c28ed1ae329d5bb238eb
ajksl2356 <ajksl2356@gmail.com>
2024-03-11 13:40:45 +0900

    add SecondActivity & image file

--- app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity2.kt
+++ app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity2.kt
@@ -1,11 +1,12 @@
-package com.lym2024.githuubtest
+package com.lym2024.githuubtest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lym2024.githuubtest.R
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_second2)
     }
 }
