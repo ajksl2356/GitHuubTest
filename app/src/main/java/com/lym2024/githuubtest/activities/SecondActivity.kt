c4dc1c82d19324c199cee6c1af1e4d95a9091658
ajksl2356 <ajksl2356@gmail.com>
2024-03-11 13:43:16 +0900

    view binding SecondActivity...

--- app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity.kt
+++ app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity.kt
@@ -2,10 +2,9 @@ package com.lym2024.githuubtest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.lym2024.githuubtest.R
 import com.lym2024.githuubtest.databinding.ActivitySecond2Binding
 
-class SecondActivity2 : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
 
     lateinit var binding: ActivitySecond2Binding
 
