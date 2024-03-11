adec712b7ee5777d2a045b00b176bf7dd2cea503
ajksl2356 <ajksl2356@gmail.com>
2024-03-11 14:04:27 +0900

    add click listener

--- app/src/main/java/com/lym2024/githuubtest/activities/MainActivity.kt
+++ app/src/main/java/com/lym2024/githuubtest/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package com.lym2024.githuubtest.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.lym2024.githuubtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        
+        binding.tv.setOnClickListener { Toast.makeText(this, "clicked textview", Toast.LENGTH_SHORT).show() }
 
 
 
