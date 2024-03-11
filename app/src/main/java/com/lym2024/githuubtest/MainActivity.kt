6588db73686c0d9fe015af103e274c36f293eca2
ajksl2356 <ajksl2356@gmail.com>
2024-03-11 13:21:33 +0900

    Initial commit

---
+++ app/src/main/java/com/lym2024/githuubtest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lym2024.githuubtest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
