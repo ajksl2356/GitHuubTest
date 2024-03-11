2e86cc66ed080e1779004be1a82712554da04b77
ajksl2356 <ajksl2356@gmail.com>
2024-03-11 13:42:41 +0900

    view binding SecondActivity...

--- app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity2.kt
+++ app/src/main/java/com/lym2024/githuubtest/activities/SecondActivity2.kt
@@ -3,10 +3,14 @@ package com.lym2024.githuubtest.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.lym2024.githuubtest.R
+import com.lym2024.githuubtest.databinding.ActivitySecond2Binding
 
 class SecondActivity2 : AppCompatActivity() {
+
+    lateinit var binding: ActivitySecond2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_second2)
+        setContentView(binding.root)
     }
 }
